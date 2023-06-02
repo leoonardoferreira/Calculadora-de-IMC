9ab4bb6f27e5936bdcb9e8c8d463f6994c1d0b6b
Leo <ferreiradasilvaleonardo7@gmail.com>
2023-06-02 20:37:30 -0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,53 @@
+import { Modal } from './modal.js';
+import { AlertError } from './alert-error.js';
+import { calculateIMC, notNumber } from './utils.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+
+form.onsubmit = event => {
+    event.preventDefault()
+    
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const weightOrHeightIsNotANumber = notNumber(weight) || notNumber(height)
+
+    if (weightOrHeightIsNotANumber) {
+        AlertError.open()
+        return;
+    }
+
+    const result = calculateIMC(weight, height)
+    displayResultMessage(result)
+}
+
+inputWeight.oninput = () => AlertError.close()
+inputHeight.oninput = () => AlertError.close()
+
+function displayResultMessage(result) {
+    const message = `Seu IMC é de ${result}`
+    
+    Modal.message.innerText = message
+    Modal.open()
+}
+
+
+
+// maneiras de criar e atribuir função a um evento
+
+// form.onsubmit = function() {
+// }
+
+// form.onsubmit = () => {
+// }
+
+// form onsubmit = handleSubmit
+
+// function handleSubmit() {
+
+// }
+
+
